f2e6565f1c6861e47c6250c90657da2d567bab9d
Romain <romain@mbprom1.home>
2026-06-22 15:22:49 +0200

    Création des routes auth et fix bug Express Request

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,3 +1,4 @@
+/// <reference path="../types/express.d.ts" />
 import type { Request, Response, NextFunction } from "express";
 import { HttpError } from "../middlewares/error";
 import { isEmail, isString } from "../helpers/validators";
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,14 +1,6 @@
 import { Response } from "express";
 import { Types } from "mongoose";
 
-declare global {
-  namespace Express {
-    interface Request {
-      token?: string;
-    }
-  }
-}
-
 export type JsonResponse<T> = Response<T>;
 export type MessageResponse = { message: string };
 export type MongoId = {
