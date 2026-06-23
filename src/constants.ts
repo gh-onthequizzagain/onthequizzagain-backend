f3a83c2ddb674253ecc35c4a5531e17218c39550
User <mathieu.jullien77190@gmail.com>
2026-06-23 12:10:40 +0200

    add prettier + add setup instructions to README

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,5 @@
-//export const PORT_SERVER = process.env.PORT;
-export const PORT_SERVER = 3000
+export const PORT_SERVER = process.env.PORT;
 
-export const PORT_BD = 27017;
 export const URI_BD = process.env.URI_BD;
 
 export enum Color {
@@ -14,4 +12,3 @@ export enum Color {
   Cyan = 36,
   White = 37,
 }
-
